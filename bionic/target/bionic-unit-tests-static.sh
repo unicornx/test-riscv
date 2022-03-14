3f6cde1e2bed282ede71f45a87581e4a8e68a19e
Chen Wang <wangchen20@iscas.ac.cn>
2022-03-14 13:43:46 +0800

    added logs after fixing fenv related issues
    plus updating scripts for running on target.
    
    Signed-off-by: Chen Wang <wangchen20@iscas.ac.cn>

--- bionic/target/bionic-unit-tests-static.sh
+++ bionic/target/bionic-unit-tests-static.sh
@@ -6,23 +6,26 @@ PATH_TESTBIN=/data/nativetest64/bionic-unit-tests-static/bionic-unit-tests-stati
 # but leave those cases may fail running or crash
 PATTERNS="\
 -\
-:fenv.feenableexcept_fegetexcept\
 :string_nofortify.memcpy:string_nofortify.memset:string_nofortify.memmove:string_nofortify.bcopy:string_nofortify.strlcat_overread:string_nofortify.strcat_overread\
 :string.memcpy:string.memset:string.memmove:string.bcopy:string.strlcat_overread:string.strcat_overread\
-:string.memmove_cache_size:string.strchr_multiple\
 "
 
 # FIXME:
-# disable fenv.feenableexcept_fegetexcept, due to I found if we run this suite, other suites after it will be run twice
 #
 # Some cases are ignored just due to they cost too long time, such as:
-# [       OK ] string_nofortify.memcpy (23709 ms)
-# [       OK ] string_nofortify.memset (26896 ms)
-# [       OK ] string_nofortify.memmove (53577 ms)
-# [       OK ] string_nofortify.bcopy (51899 ms)
-# [       OK ] string_nofortify.strcat_overread (17148 ms)
-# upon 5 cases the same for string
-# string.memmove_cache_size:string.strchr_multiple only slow for string, but not for string_nofortify, don't know why
+# [  SLOW    ] string_nofortify.memcpy (27730 ms, exceeded 2000 ms)
+# [  SLOW    ] string_nofortify.memset (31369 ms, exceeded 2000 ms)
+# [  SLOW    ] string_nofortify.memmove (63412 ms, exceeded 2000 ms)
+# [  SLOW    ] string_nofortify.bcopy (61589 ms, exceeded 2000 ms)
+# [  SLOW    ] string_nofortify.strlcat_overread (18611 ms, exceeded 2000 ms)
+# [  SLOW    ] string_nofortify.strcat_overread (17586 ms, exceeded 2000 ms)
+#
+# [  SLOW    ] string.memcpy (27700 ms, exceeded 2000 ms)
+# [  SLOW    ] string.memset (31375 ms, exceeded 2000 ms)
+# [  SLOW    ] string.memmove (63300 ms, exceeded 2000 ms)
+# [  SLOW    ] string.bcopy (61312 ms, exceeded 2000 ms)
+# [  SLOW    ] string.strcat_overread (18072 ms, exceeded 2000 ms)
+# [  SLOW    ] string.strlcat_overread (18635 ms, exceeded 2000 ms)
 
 
 if [ $# -ne 0 ]; then
--- bionic/target/bionic-unit-tests.sh
+++ bionic/target/bionic-unit-tests.sh
@@ -6,23 +6,12 @@ PATH_TESTBIN=/data/nativetest64/bionic-unit-tests/bionic-unit-tests
 # but leave those cases may fail running or crash
 PATTERNS="\
 -\
-:fenv.feenableexcept_fegetexcept\
 :string_nofortify.memcpy:string_nofortify.memset:string_nofortify.memmove:string_nofortify.bcopy:string_nofortify.strlcat_overread:string_nofortify.strcat_overread\
 :string.memcpy:string.memset:string.memmove:string.bcopy:string.strlcat_overread:string.strcat_overread\
-:string.memmove_cache_size:string.strchr_multiple\
 "
 
 # FIXME:
-# disable fenv.feenableexcept_fegetexcept, due to I found if we run this suite, other suites after it will be run twice
-#
-# Some cases are ignored just due to they cost too long time, such as:
-# [       OK ] string_nofortify.memcpy (23709 ms)
-# [       OK ] string_nofortify.memset (26896 ms)
-# [       OK ] string_nofortify.memmove (53577 ms)
-# [       OK ] string_nofortify.bcopy (51899 ms)
-# [       OK ] string_nofortify.strcat_overread (17148 ms)
-# upon 5 cases the same for string
-# string.memmove_cache_size:string.strchr_multiple only slow for string, but not for string_nofortify, don't know why
+# refer to ./bionic-unit-tests-static.sh for details for PATTERNS
 
 
 if [ $# -ne 0 ]; then
