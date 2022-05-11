5e362450df0dcb3033778143c886f900e6a46a08
Chen Wang <wangchen20@iscas.ac.cn>
2022-05-11 14:52:13 +0800

    enable test string/mem cases after we optimized mem*

--- bionic/target/bionic-unit-tests-static.sh
+++ bionic/target/bionic-unit-tests-static.sh
@@ -2,35 +2,10 @@
 
 PATH_TESTBIN=/data/nativetest64/bionic-unit-tests-static/bionic-unit-tests-static
 
-# we ignore those cases which will make running blocked/long-running-time
-# but leave those cases may fail running or crash
-PATTERNS="\
--\
-:string_nofortify.memcpy:string_nofortify.memset:string_nofortify.memmove:string_nofortify.bcopy:string_nofortify.strlcat_overread:string_nofortify.strcat_overread\
-:string.memcpy:string.memset:string.memmove:string.bcopy:string.strlcat_overread:string.strcat_overread\
-"
-
-# FIXME:
-#
-# Some cases are ignored just due to they cost too long time, such as:
-# [  SLOW    ] string_nofortify.memcpy (27730 ms, exceeded 2000 ms)
-# [  SLOW    ] string_nofortify.memset (31369 ms, exceeded 2000 ms)
-# [  SLOW    ] string_nofortify.memmove (63412 ms, exceeded 2000 ms)
-# [  SLOW    ] string_nofortify.bcopy (61589 ms, exceeded 2000 ms)
-# [  SLOW    ] string_nofortify.strlcat_overread (18611 ms, exceeded 2000 ms)
-# [  SLOW    ] string_nofortify.strcat_overread (17586 ms, exceeded 2000 ms)
-#
-# [  SLOW    ] string.memcpy (27700 ms, exceeded 2000 ms)
-# [  SLOW    ] string.memset (31375 ms, exceeded 2000 ms)
-# [  SLOW    ] string.memmove (63300 ms, exceeded 2000 ms)
-# [  SLOW    ] string.bcopy (61312 ms, exceeded 2000 ms)
-# [  SLOW    ] string.strcat_overread (18072 ms, exceeded 2000 ms)
-# [  SLOW    ] string.strlcat_overread (18635 ms, exceeded 2000 ms)
-
-
 if [ $# -ne 0 ]; then
-    PATTERNS=$1
+    $PATH_TESTBIN --gtest_filter=$1
+else
+    $PATH_TESTBIN
 fi
 
-$PATH_TESTBIN --gtest_filter=$PATTERNS
 
--- bionic/target/bionic-unit-tests.sh
+++ bionic/target/bionic-unit-tests.sh
@@ -2,25 +2,14 @@
 
 PATH_TESTBIN=/data/nativetest64/bionic-unit-tests/bionic-unit-tests
 
-# we ignore those cases which will make running blocked/long-running-time
-# but leave those cases may fail running or crash
-PATTERNS="\
--\
-:string_nofortify.memcpy:string_nofortify.memset:string_nofortify.memmove:string_nofortify.bcopy:string_nofortify.strlcat_overread:string_nofortify.strcat_overread\
-:string.memcpy:string.memset:string.memmove:string.bcopy:string.strlcat_overread:string.strcat_overread\
-"
-
-# FIXME:
-# refer to ./bionic-unit-tests-static.sh for details for PATTERNS
-
-
-if [ $# -ne 0 ]; then
-    PATTERNS=$1
-fi
-
 # we use an empty ld.config.txt, but we use LD_LIBRARY_PATH to help linker
 # to serach for the .so files
 # search /apex/com.android.i18n/lib64/ for libicu.so
-LD_LIBRARY_PATH=/system/lib64/bootstrap:/apex/com.android.i18n/lib64 \
-$PATH_TESTBIN --gtest_filter=$PATTERNS
+export LD_LIBRARY_PATH=/system/lib64/bootstrap:/apex/com.android.i18n/lib64
+if [ $# -ne 0 ]; then
+    $PATH_TESTBIN --gtest_filter=$1
+else
+    $PATH_TESTBIN
+fi
+
 
